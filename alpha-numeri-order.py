18cd8b7f115447391899cf1ae41a6df64d2e6609
Dhruv <dhruvvvbansal@gmail.com>
2022-06-16 11:07:34 +0530

    added second approch to solve alha-muneric-sort problem

--- alpha-numeri-order.py
+++ alpha-numeri-order.py
@@ -9,3 +9,16 @@ for i in input_string:
 
 string_final = "".join(sorted(list_alpha)) + "".join(sorted(list_number))
 print(string_final)
+
+
+# second approach#
+list_alpha = []
+list_number = []
+for ch in input_string:
+    if ch.isalpha():
+        list_alpha.append(ch)
+    else:
+        list_number.append(ch)
+
+string_final = "".join(sorted(list_alpha)) + "".join(sorted(list_number))
+print(string_final)
